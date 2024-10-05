38d80fb8d9a1e2397383fe574761e9d6e756d054
Olayinka-2 <olayinkaisrael802@gmail.com>
2024-10-05 04:05:49 +0100

    Add linkedList class

---
+++ script.js
@@ -0,0 +1,14 @@
+class LinkedList {
+   head = null;
+   size = 0;
+}
+
+class Node {
+   constructor(data) {
+      this.data = data;
+      this.next = null;
+   }
+}
+
+// let man = new LinkedList();
+// console.log(man);
