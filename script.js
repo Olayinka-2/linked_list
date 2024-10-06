74f8426a6b7f9dffea23144920f95b8c0433d5d4
Olayinka-2 <olayinkaisrael802@gmail.com>
2024-10-06 22:43:05 +0100

    Add the pop method

--- script.js
+++ script.js
@@ -57,10 +57,48 @@ LinkedList.prototype.tailItem = function() {
    return currentHead;
 }
 
+LinkedList.prototype.at = function(number) {
+   let currentHead = this.head;
+   let count = 0;
+
+   if(number == 0) {
+      return currentHead.data;
+   }
+
+   while(true) {
+      currentHead = currentHead.next;
+      count++;
+
+      if(count == number) {
+         break;
+      }
+   }
+   return currentHead.data;
+}
+
+LinkedList.prototype.pop = function() {
+   let currentHead = this.head;
+
+   if(this.size == 1) {
+      this.head = null;
+      this.size--;
+      return;
+   }
+
+   while(currentHead.next.next !== null) {
+      currentHead = currentHead.next;
+   }
+   currentHead.next = currentHead.next.next;
+   this.size--;
+}
+
+
 let man = new LinkedList();
 man.append(3);
 man.append(4);
 man.prepend(5);
 // console.log(man.headItem());
-console.log(man.tailItem());
-// console.log(man);
+// console.log(man.tailItem());
+// man.pop();
+// console.log(man.at(4));
+console.log(man);
