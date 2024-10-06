a098a9d6fa9b34c589638213f381b6f3c3c7377a
Olayinka-2 <olayinkaisrael802@gmail.com>
2024-10-06 22:15:56 +0100

    feat: Add head and tail function

--- script.js
+++ script.js
@@ -10,5 +10,57 @@ class Node {
    }
 }
 
-// let man = new LinkedList();
+LinkedList.prototype.prepend = function(value) {
+   if(this.head === null) {
+      this.head = new Node(value);
+   } else {
+      const temp = this.head;
+      this.head = new Node(value);
+      this.head.next = temp;
+   }
+   this.size++;
+}
+
+LinkedList.prototype.append = function(value) {
+   if(this.head === null) {
+      this.head = new Node(value);
+      this.size++;
+      return;
+   }
+
+   let currentHead = this.head;
+
+   while(currentHead.next !== null) {
+      currentHead = currentHead.next;
+   }
+
+   currentHead.next = new Node(value);
+
+   this.size++;
+}
+
+LinkedList.prototype.sizes = function() {
+   return this.size;
+}
+
+LinkedList.prototype.headItem = function() {
+   return this.head;
+}
+
+LinkedList.prototype.tailItem = function() {
+   let currentHead = this.head;
+
+   while(currentHead.next !== null) {
+      currentHead = currentHead.next
+   }
+
+   return currentHead;
+}
+
+let man = new LinkedList();
+man.append(3);
+man.append(4);
+man.prepend(5);
+// console.log(man.headItem());
+console.log(man.tailItem());
 // console.log(man);
